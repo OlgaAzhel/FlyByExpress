3de8df8a1f0ae86393c39271fff930fcfa0c7ae4
Olga Azhel <olga.azhel@gmail.com>
2023-03-02 15:02:20 -0500

    form validation add

--- controllers/offers.js
+++ controllers/offers.js
@@ -3,7 +3,9 @@ const User = require('../models/user');
 const Review = require('../models/review');
 
 function newOffer(req, res) {
-    res.render('offers/new', { title: 'Add New Delivery offer' });
+    let listOfErrors = []
+    res.render('offers/new', { title: 'Add New Delivery offer',
+        listOfErrors });
 }
 
 
@@ -16,16 +18,32 @@ function create(req, res) {
     req.body.mail = !!req.body.mail
     req.body.electronics = !!req.body.electronics
 
+    // check if each of theree dates is a real date
+    // check if the departure date is within 1 year from current date
+    // check if package accept date is earlier than  or equal departure date
+    // check if departure date is earlier or equal available date
+
     const offer = new Offer(req.body)
 
-    // User.findById(req.user._id, function (err, theUser) {
-    //     theUser.offers.push(offer)
-    //     theUser.save(function (err) {
-    //     })
-    // })
-
     offer.save(function (err) {
-        res.redirect('offers/')
+        // https://stackoverflow.com/questions/61056021/improve-mongoose-validation-error-handling
+        if (err.name === "ValidationError") {
+            console.log("THIS IS ERROR OBJECT:", err, err.name)
+            
+            let listOfErrors = []
+            Object.keys(err.errors).forEach((key) => {
+                
+                listOfErrors.push(err.errors[key].message);
+
+            });
+            console.log(listOfErrors)
+            res.render('offers/new', {
+                title: 'Add New Delivery offer',
+                listOfErrors
+            });
+        } else {
+            res.redirect('offers/')
+        }
     })
 
 }
@@ -37,10 +55,10 @@ function show(req, res) {
             User.findById(offer.creator._id, function (err, offerCreator) {
                 // let creatorName = offerCreator.name
                 Review.find({ offerCreator: offerCreator }, function (err, couriersReviews) {
-                    console.log("REVIEWS FOR THIS COURIER:",offerCreator.name, couriersReviews)
+                    console.log("REVIEWS FOR THIS COURIER:", offerCreator.name, couriersReviews)
                     let totalScore = 0
                     let couriersRating
-                    if(couriersReviews) {
+                    if (couriersReviews) {
                         couriersReviews.forEach((review) => {
                             totalScore += review.communication
                             totalScore += review.cost
--- models/offer.js
+++ models/offer.js
@@ -5,32 +5,96 @@ const Schema = mongoose.Schema;
 const offerSchema = new Schema({
     direction: {
         type: String,
-        required: true
+        required: true,
     },
     departureCity: {
         type: String,
-        required: true
+        
+        required: true,
+        validate: {
+            validator: function(input) {
+                return input.length <= 50
+            },
+            message: "City name must be not longer than 50 characters"
+        }
     },
-    transitCity: String,
+    transitCity: {
+        type:String,
+        validate: {
+            validator: function (input) {
+                return input.length <= 50
+            },
+            message: "City name must be not longer than 50 characters"
+        }
+    },
+
     destinationCity: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function (input) {
+                return input.length <= 50
+            },
+            message: "City name must be not longer than 50 characters"
+        }
     },
     departureDate: {
         type: Date,
-        required: true
+        required: true,
+        validate: {
+            validator: function (v) {
+                return (
+                    v && // check that there is a date object
+                    v.getTime() > Date.now() + 24 * 60 * 60 * 1000 &&
+                    v.getTime() < Date.now() + 365 * 24 * 60 * 60 * 1000
+                );
+            },
+            message:
+                "A departure must be not earlier than 1 day before and not later than 1 year after current date."
+    }
     },
+
     cutoffDate: {
         type: Date,
-        required: true
+        required: true,
+        validate: {
+            validator: function (v) {
+                return (
+                    v && // check that there is a date object
+                    v.getTime() <= this.departureDate.getTime() &&
+                    v.getTime() >= Date.now()
+                );
+            },
+            message:
+                "A package accept last day must be not later than departure date and not eralier than current date."
+        }
     },
     availDate: {
         type: Date,
-        required: true
+        required: true,
+        validate: {
+            validator: function (v) {
+                return (
+                    v && // check that there is a date object
+                    v.getTime() >= this.departureDate.getTime() &&
+                    v.getTime() <= this.departureDate.getTime() + 30 * 24 * 60 * 60 * 1000
+                );
+            },
+            message:
+                "Package arrived and available day must be not earlier than departure date and not later than 30 days after departure date."
+        }
     },
     mail: Boolean,
     electronics: Boolean,
-    info: String,
+    info: {
+        type:String,
+        validate: {
+            validator: function (input) {
+                return input.length <= 200
+            },
+            message: "Additional info must be not longer than 50 characters"
+        }
+},
     creator: {
         type: Schema.Types.ObjectId,
         ref: 'User',
